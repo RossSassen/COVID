2ee23ce9914f1e7d1162c15d3fead300ee3a28ec
RossSassen <63886180+RossSassen@users.noreply.github.com>
2020-04-18 01:21:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,54 @@
 const context = {
-    title: 'CORONAVIUS Grocery Cleaning Guide',
-    body: 'Everyone is having a difficult time deciding how much risk they are willing to take during the current COVID-19 pandemic.  This guide was created to help people minimize the risk associated with bringing groceries into their home.  Groceries seem like they should be safe, but from production to transportation to sitting on shelves, there are dozens of opportunities for COVID-19 to attach to the surface of your groceries.  While we cannot completely eliminate the risk of infection, we can minimize it!  Please use the following guide to help your household.',
-    instruments: [
-      {
-        image: '',
-        name: '',
-        description: 'A piano welcomed to the 21th century. Pianists celebrate the compact form factor and the diversity of synthesized rhythms all in one masterful musical machine.',
-        price: '$1,999.00',
-        sale: '$1,699.89'
-      },
-      {
-        image: '',
-        name: 'Electric Guitar',
-        description: 'Join the legends of the \'50s and \'60s when the marriage of guitar and electricity created the most influential instrument of a generation. Note: picks sold separately.',
-        price: '$599.99'
-      },
-      {
-        image: 'https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/bass-guitar.png',
-        name: 'Bass Guitar',
-        description: 'Experience the embodiment of funkadelic frequencies that is the bass guitar. Let the deep low notes of the bass guitar resonate with heartbeats everywhere.',
-        price: '$624.99'
-      },
-      {
-        image: 'https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/drum-kit.png',
-        name: 'Drum Kit',
-        description: 'Ever thought, "one instrument isn\'t enough?" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.',
-        price: '$649.00',
-        sale: '$349.00'
-      },
-      {
-        image: 'https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/violin.png',
-        name: 'Violin',
-        description: 'A versatile that is suited for any and all occasions. Those wearing tuxedos can strum together a classic. Others who prefer overalls can call it a fiddle and play some folk songs.',
-        price: '$245.00'
-      }
-    ]
-  };
-  const templateElement = document.getElementById("templateHB");
-  const templateSource = templateElement.innerHTML
-  const template = Handlebars.compile(templateSource);
-  const compiledHtml = template(context);
-  document.getElementById('information').innerHTML = compiledHtml;
+  title: 'CORONAVIUS Grocery Cleaning Guide',
+  body: 'Everyone is having a difficult time deciding how much risk they are willing to take during the current COVID-19 pandemic.  This guide was created to help people minimize the risk associated with bringing groceries into their home.  Groceries seem like they should be safe, but from production to transportation to sitting on shelves, there are dozens of opportunities for COVID-19 to attach to the surface of your groceries.  While we cannot completely eliminate the risk of infection, we can minimize it!  Please use the following guide to help your household.',
+  instruments: [
+    {
+      image: 'https://assets.fishersci.com/TFS-Assets/CCG/Dupont-Personal-Protection/product-images/31hJnaRnwcL.jpg-650.jpg',
+      name: 'Cleaning Station',
+      description: 'It is important to set up a single area for cleaning your groceries.  This will limit the possible areas for cross contamination and streamline the porcess.',
+      price: ''
+    },
+    {
+      image: 'https://cdn.thomasnet.com/insights-images/embedded-images/bea03b4e-0494-4a04-a58c-13c486bec48d/fbbdc284-997d-446d-b460-54e59319b937/FullHD/shutterstock_1607293264-min.jpg',
+      name: 'Gloves, Disinfectant Wipes or Spray',
+      description: 'Using nitrile or latex gloves during the cleaning process will both protect your skin from cleaning chemicals and prevent the virus from getting on your hands.  If you do not have gloves, thourghly wash your hands before and after cleaning your groceries and do not touch your face while conducting the cleaning.',
+      price: '',
+    },
+    {
+      image: 'https://www.meijer.com/content/dam/meijer/product/0070/88/2068/69/0070882068698_1_A1C1_1200.png',
+      name: 'Packaged Meat',
+      description: 'Packaged meat presents a challenge since it must enter your refridgerator immediately.  Wipe down all sides of the packaging with a disinfecting wipe, or apply a mist of disinfecting spray from a bottled product to all sides and then wipe it with a dry towel.',
+      price: ''
+    },
+    {
+      image: 'https://www.seriouseats.com/images/2014/01/20140122-taste-test-nuggets-overhead-primary.jpg',
+      name: 'Frozen Food',
+      description: 'Frozen food generally comes in a plastic container, so the process is just like the one used for packaged meat.  Wipe down all sides of the packaging with a disinfecting wipe, or apply a mist of disinfecting spray from a bottled product to all sides and then wipe it with a dry towel.',
+      price: ''
+    },
+    {
+      image: 'https://www.minimus.biz/Images/F25-2509105-4100bg-1.jpg',
+      name: 'Cardboard Packaged Food',
+      description: 'The coronavirus has a relatively short life span on cardboard items.',
+      price: ''
+    }
+  ]
+};
+const templateElement = document.getElementById("templateHB");
+const templateSource = templateElement.innerHTML
+const template = Handlebars.compile(templateSource);
+const compiledHtml = template(context);
+document.getElementById('information').innerHTML = compiledHtml;
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <p1>{new Date()}</p1>, 
+  document.getElementById('app')
+  );
+ */
+let curDate = document.createElement('p');
+curDate.id = 'info'; 
+curDate.innerHTML = new Date();
+document.getElementById('app').appendChild(curDate);
+
